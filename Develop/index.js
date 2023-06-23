802e038689db77a414ecd279ba7228208572c1da
joshualam071522 <joshualam071522@gmail.com>
2023-06-23 00:01:36 -0700

    added prompt questions from inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,42 @@
-// TODO: Include packages needed for this application
+//* packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+//* questions to ask the user 
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What was your motivation? Why did you build this project? What did you learn?'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How did you install your project?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this project?'
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'List collaborators with links to Github Profiles'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please choose a license for your project'
+        ['MIT', 'Apache 2.0', 'GNU GPL v3', 'None']
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
